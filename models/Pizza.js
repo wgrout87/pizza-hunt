339857918675e02714faeedb152a6cf239e01aa9
Will G <wgrout87@gmail.com>
2022-07-06 14:46:23 -0600

    added reply functionality

--- models/Pizza.js
+++ models/Pizza.js
@@ -34,7 +34,7 @@ const PizzaSchema = new Schema({
     });
 
 PizzaSchema.virtual('commentcount').get(function () {
-    return this.comments.length;
+    return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
 });
 
 const Pizza = model('Pizza', PizzaSchema);
--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controllers');
+const { addComment, removeComment, addReply, removeReply } = require('../../controllers/comment-controllers');
 
 // POST route for comments on pizza with pizzaId
 router.route('/:pizzaId').post(addComment);
 
 // DELETE route for comment with commentId on pizza with pizzaId
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router.route('/:pizzaId/:commentId').put(addReply).delete(removeComment);
+
+// DELETE router for a reply with replyId on comment with commentId on pizza with pizzaId
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
 module.exports = router;
